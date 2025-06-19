1093c90f247e6d9cf10a9a9a99c3d1956ec307b7
Juan Amaya <97049133+jamayak@users.noreply.github.com>
2025-06-19 15:02:41 -0500

    preparar para producción

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,17 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {DatePipe} from '@angular/common';
 import {Button} from 'primeng/button';
 import {RouterLink} from '@angular/router';
 
 @Component({
-  selector: 'app-home',
+    selector: 'app-home',
     imports: [
-        DatePipe,
         Button,
         RouterLink
     ],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+    production: true,
+    urlApi: 'https://api.mundodefeplaya.org/api',
+};
