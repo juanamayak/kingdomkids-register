dc610cc629f7fdcec5261e6093946c9fb41cddb6
Juan Amaya <amayajuan95@gmail.com>
2025-07-15 01:39:12 -0500

    bug: back camera scanner

--- src/app/pages/scanner/scanner.component.ts
+++ src/app/pages/scanner/scanner.component.ts
@@ -1,16 +1,17 @@
-import {Component, inject, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, inject, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {Router} from '@angular/router';
 import {NgxScannerQrcodeComponent} from 'ngx-scanner-qrcode';
+import {Html5Qrcode} from 'html5-qrcode';
 
 @Component({
-  selector: 'app-scanner',
-  imports: [
-      NgxScannerQrcodeComponent
-  ],
-  templateUrl: './scanner.component.html',
-  styleUrl: './scanner.component.scss'
+    selector: 'app-scanner',
+    imports: [
+
+    ],
+    templateUrl: './scanner.component.html',
+    styleUrl: './scanner.component.scss'
 })
-export class ScannerComponent implements OnInit{
+export class ScannerComponent implements AfterViewInit, OnDestroy {
 
     @ViewChild('scanner', {static: true}) cameraScanner: any;
 
@@ -19,12 +20,53 @@ export class ScannerComponent implements OnInit{
     // Usa facingMode 'environment' para cámara trasera
     cameraConstraints: MediaStreamConstraints = {
         video: {
-            facingMode: { ideal: 'environment' } // o exact si quieres forzar
+            facingMode: {exact: 'environment'} // o exact si quieres forzar
         }
     };
 
-    ngOnInit(): void {
+    devices: MediaDeviceInfo[] = [];
+    selectedDeviceId: string | null = null;
+
+    private html5QrCode!: Html5Qrcode;
+
+    async ngAfterViewInit() {
+        this.html5QrCode = new Html5Qrcode("reader");
+
+        const devices = await Html5Qrcode.getCameras();
+        const backCamera = devices.find(d => d.label.toLowerCase().includes('back')) || devices[0];
+
+        this.html5QrCode.start(
+            backCamera.id,
+            {
+                fps: 10,
+                qrbox: {width: 250, height: 250}
+            },
+            (decodedText) => {
+                // this.html5QrCode.stop();
+                this.router.navigate([decodedText])
+                // navegar o manejar el resultado
+            },
+            (errorMessage) => {
+                // console.warn("No se detectó QR", errorMessage);
+            }
+        );
+    }
+
+    ngOnDestroy() {
+        this.html5QrCode?.stop().catch(() => {
+        });
+    }
+
+    /*async ngOnInit() {
         this.cameraScanner.start();
+        // ✅ Aplica cámara trasera después de iniciar
+        await this.cameraScanner.applyConstraints({
+            video: {
+                facingMode: { ideal: 'environment' }  // o ideal
+            }
+        });
+
+        console.log(this.cameraScanner);
     }
 
     captureQRData(event: any){
@@ -32,5 +74,5 @@ export class ScannerComponent implements OnInit{
             this.cameraScanner.stop()
             this.router.navigate([event[0].value])
         }
-    }
+    }*/
 }
